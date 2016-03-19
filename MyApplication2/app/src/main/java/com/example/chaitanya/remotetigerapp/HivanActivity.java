61847c1ffca601389a64c7dceee1c60890928f07
Hivan <ehivan24@hotmail.com>
2016-03-19 16:33:23 -0400

    New class added

---
+++ MyApplication2/app/src/main/java/com/example/chaitanya/remotetigerapp/HivanActivity.java
@@ -0,0 +1,13 @@
+package com.example.chaitanya.remotetigerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HivanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hivan);
+    }
+}
