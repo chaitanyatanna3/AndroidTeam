349c841be8789d1173c57e3ad845150548b4196c
Hivan <ehivan24@hotmail.com>
2016-03-25 15:31:48 -0400

    Button Image

--- MyApplication2/app/src/main/java/com/example/chaitanya/remotetigerapp/HivanActivity.java
+++ MyApplication2/app/src/main/java/com/example/chaitanya/remotetigerapp/HivanActivity.java
@@ -2,6 +2,8 @@ package com.example.chaitanya.remotetigerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class HivanActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HivanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hivan);
+
+    }
+
+    public void testOpen(View view) {
+        Toast.makeText(this, "!", Toast.LENGTH_SHORT).show();
     }
 }
